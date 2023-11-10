0804829a65524d966f4c5da83a7184a0bb49f91c
zhangguoxiang <Yunjikeji123>
2023-11-10 10:11:39 +0800

    init 仓库

---
+++ src/test/java/com/example/communitydemo/CommunityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.communitydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
